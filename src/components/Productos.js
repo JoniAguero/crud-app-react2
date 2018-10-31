8a454fb4d79b652cffdcf7c18aafa74b997620fa
JoniAguero <jaguero@trimix.com.ar>
2018-10-31 20:50:38 -0300

    Components added

---
+++ src/components/Productos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Productos extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Productos
